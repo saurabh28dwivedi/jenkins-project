87b0565819264d7cd991cc6bd56ff2c43e31e838
Saurabh Dwivedi <saurabhd3@verifone.com>
2022-12-08 18:30:19 +0530

    first commit

---
+++ src/main/java/jenkins/jankins/JankinsApplication.java
@@ -0,0 +1,13 @@
+package jenkins.jankins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JankinsApplication.class, args);
+	}
+
+}
